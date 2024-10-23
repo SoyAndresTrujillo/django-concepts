223395884f2c442929e7d5f55296d3666dac7eee
soyandrestrujillo <andymania.1301@gmail.com>
2024-10-22 21:14:06 -0500

    feat: relation 1:N

--- my_first_project/my_first_app/models.py
+++ my_first_project/my_first_app/models.py
@@ -10,3 +10,29 @@ class Car(models.Model):
     # Return the title of the car for show in shell
     def __str__(self):
         return f"{self.title} - {self.year} - {self.color}"
+
+
+class Publisher(models.Model):
+    name = models.TextField(max_length=200)
+    address = models.TextField(max_length=250, null=True)
+    # city = models.TextField(max_length=250, null=True)
+    # state_province = models.TextField(max_length=250, null=True)
+    # country = models.TextField(max_length=250, null=True)
+
+    # Return the name of the publisher for show in shell
+    def __str__(self):
+        return f"{self.name} - {self.address}"
+
+
+class Book(models.Model):
+    title = models.TextField(max_length=200)
+    publication_date = models.DateField()
+    # ForeignKey is a relationship between two models
+    # what is on_delete=models.CASCADE?
+    # CASCADE: If the referenced object is deleted, also delete the object that has the ForeignKey.
+    # If publisher is deleted, also delete the book
+    # also, on_delete have more options.
+    publisher = models.ForeignKey(Publisher, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.title} - {self.publication_date} - {self.publisher}"
