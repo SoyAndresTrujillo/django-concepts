56d87b07eec2c81619df3e9c45019ebbc9644762
soyandrestrujillo <andymania.1301@gmail.com>
2024-10-22 20:45:16 -0500

    feat: insert date with django

--- my_first_project/my_first_app/models.py
+++ my_first_project/my_first_app/models.py
@@ -4,3 +4,8 @@ from django.db import models
 # Create your models here.
 class Car(models.Model):
     title = models.TextField(max_length=250)
+    year = models.TextField(max_length=4, null=True)
+
+    # Return the title of the car for show in shell
+    def __str__(self):
+        return self.title
