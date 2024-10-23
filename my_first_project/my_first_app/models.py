7fcd0fd6624c05fb71c52a92ca2383406dc17bef
soyandrestrujillo <andymania.1301@gmail.com>
2024-10-22 20:48:48 -0500

    feat: add color field to model Car

--- my_first_project/my_first_app/models.py
+++ my_first_project/my_first_app/models.py
@@ -5,7 +5,8 @@ from django.db import models
 class Car(models.Model):
     title = models.TextField(max_length=250)
     year = models.TextField(max_length=4, null=True)
+    color = models.TextField(max_length=20, null=True)
 
     # Return the title of the car for show in shell
     def __str__(self):
-        return self.title
+        return f"{self.title} - {self.year} - {self.color}"
